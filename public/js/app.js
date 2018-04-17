5e4426f52edd4d19098e8bab05704e25e02526aa
George Hattrell <ghattrell@gmail.com>
2018-04-17 13:19:41 +0100

    some javascript to fetch the entries and append them using jQuery

--- public/js/app.js
+++ public/js/app.js
@@ -86,7 +86,7 @@ module.exports = __webpack_require__(218);
 
 "use strict";
 $(document).ready(function () {
-  var base_url = 'http://127.0.0.1:8000/';
+  var base_url = 'http://34.244.240.46/';
 
   var loadEntries = function loadEntries() {
     $.ajax({
@@ -96,8 +96,7 @@ $(document).ready(function () {
       },
       success: function success(data) {
         $.each(data.payload, function (index, entry) {
-          console.log(entry);
-          var $title = $('<h3>').text(entry.first_name + ' ' + entry.last_name);
+          var $title = $('<h3>').text(entry.full_name);
           var $message = $('<p>').text(entry.message);
           $('.entries').append($title).append($message);
         });
